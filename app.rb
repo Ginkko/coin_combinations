f183ae8070f3d183da7ae378372d503fe5975809
Toby Alden & Alec Arme <student@epicodus.com>
2015-04-23 11:12:45 -0700

    Complete HTML code for index, layout, and coin_results. Complete app.rb. Passes all specs. Manually tested web page successfully.

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
 require("sinatra")
+require("sinatra/reloader")
 require("./lib/coin_combinations")
-also_load("lib/**/*.rb")
+require("./lib/coin_array_to_string")
+also_reload("/lib/**/*.rb")
+
+get('/') do
+  erb(:index)
+end
+
+get('/coin_results') do
+  @coin_results_string = params.fetch("cash_input").to_f().coin_combinations().coin_array_to_string()
+  erb(:coin_results)
+end
