aadf1fb7064c2bd4d55f4c9d0549f66679308ff4
Toby Alden & Alec Arme <student@epicodus.com>
2015-04-23 09:52:06 -0700

    Adds spec for nickel check. Passes spec. Refactored initial array to always contain 4 elements.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,10 +1,16 @@
 class Float
   define_method(:coin_combinations) do
-    coins_to_return = []
+    coins_to_return = [0, 0, 0, 0]
 
     total_cent_value = self.*(100).to_i()
-    coins_to_return.push(total_cent_value)
-    
+
+    while total_cent_value > 5
+      coins_to_return[1] += 1
+      total_cent_value -= 5
+    end
+      coins_to_return[0] = total_cent_value
+
+      coins_to_return
 
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -4,6 +4,10 @@ require('pry')
 
 describe("Float#coin_combinations") do
   it("For input values < $0.05, coin_combinations returns the same value in pennies") do
-    expect(0.05.coin_combinations()).to(eq([5]))
+    expect(0.05.coin_combinations()).to(eq([5, 0, 0, 0]))
+  end
+
+  it("For input values < $0.09, coin_combinations returns the same value in nickels and pennies") do
+    expect(0.09.coin_combinations()).to(eq([4, 1, 0, 0]))
   end
 end
